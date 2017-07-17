9bdb7436b4f8455d76a347a6f7ff5112e4f93cde
r3cebarnett <mauralanbarn@gmail.com>
2017-07-17 13:46:46 -0400

    Added score reset and input check. Closes #1, closes #2

--- rps.py
+++ rps.py
@@ -1,10 +1,11 @@
 import random
 
 print("""
-Rock, Paper, Scissors written by Maurice Barnett, v1.0
+Rock, Paper, Scissors written by Maurice Barnett, v1.1
 Simply type "r", "p", or "s" to pick, you can keep playing until you keyboard
-interrupt or until you enter "q" when prompted. It also keeps score. Leave an
-issue on the github for "casino" under r3cebarnett if there are any issues
+interrupt or until you enter "n" when prompted. It also keeps score. Hit "r"
+instead of "y" or "n" at the end to reset the score and play again. Leave an
+issue on the github for "casino" under r3cebarnett if there are any issues.
 """)
 brk = 'y'
 cpuScore = 0
@@ -12,6 +13,8 @@ playerScore = 0
 
 while brk != 'n':
     playerInput = input('(R)ock, (P)aper, or (S)cissors?\n').upper()
+    while playerInput not in ['R','P','S']:
+        playerInput = input('Please try again (R/P/S): ')
     cpuInput = random.choice(['R','P','S'])
 
     if playerInput == 'R':
@@ -33,4 +36,7 @@ while brk != 'n':
     print('Player used', playerInput, 'and CPU used', cpuInput)
     print('Score: Player', playerScore, '-', cpuScore, 'CPU\n')
 
-    brk = input('Would you like to play again? (y/n): ').lower()
+    brk = input('Would you like to play again? (y/n/r): ').lower()
+    if brk == 'r':
+        cpuScore = 0
+        playerScore = 0
